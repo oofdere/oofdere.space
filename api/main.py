37641dada2d614613074159a351ead5d75e7a012
oofdere <43102807+oofdere@users.noreply.github.com>
2021-12-09 19:24:01 -0800

    api: add /quote endpoint

--- api/main.py
+++ api/main.py
@@ -1,16 +1,27 @@
 from typing import Optional
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
-
-@app.get("/visit")
-def visits():
-    # TODO: Count each request to visits() and return the total
-    return {"not yet implemented": "not yet implemented"}
 
 @app.get("/quote")
 def quote():
     # TODO: Return a random quote
+    quote = random.choice(open('quotes.txt').readlines())
+    url = None
+    attrib = None
+    if " - " in quote:
+        split = quote.split(" - ")
+        quote = split[0]
+        if "http" in split[1]:
+            url = split[1]
+        else:
+            attrib = split[1]
+    return {"quote": quote, "url": url, "attrib": attrib}
+
+@app.get("/visit")
+def visits():
+    # TODO: Count each request to visits() and return the total
     return {"not yet implemented": "not yet implemented"}
 
 @app.get("/status")
